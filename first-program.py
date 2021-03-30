a1dc2b64e21985bd1478eaac4099331ea63c5bb0
ddraughn <81645285+ddraughn@users.noreply.github.com>
2021-03-30 14:08:08 -0400

    Create first-program.py

---
+++ first-program.py
@@ -0,0 +1,11 @@
+# My First Program
+print("Hello World")
+name = "Ricky"
+print(name)
+
+# Taking user age as input and checking if they're 18 or older
+age = int(input("Please input your age"))
+if(age >= 18):
+    print("Adult")
+else:
+    print("less than 18")
